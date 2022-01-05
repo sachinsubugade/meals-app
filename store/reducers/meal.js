a407a914d2eb55a5a37da08ae94f11bcd68da890
mesachin <74847785+mepansachin@users.noreply.github.com>
2022-01-05 19:30:47 +0530

    updated files

--- store/reducers/meal.js
+++ store/reducers/meal.js
@@ -1,4 +1,5 @@
 import { MEALS } from "../../data/dummy-data";
+import { TOGGLE_FAVORITE, SET_FILTERS } from "../actions/meals";
 
 const initialState = {
   meals: MEALS,
@@ -7,6 +8,41 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_FAVORITE:
+      const existingIndex = state.favoriteMeals.findIndex(
+        (meal) => meal.id === action.mealId
+      );
+      if (existingIndex >= 0) {
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return { ...state, favoriteMeals: updatedFavMeals };
+      } else {
+        const meal = state.meals.find((meal) => meal.id === action.mealId);
+        return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
+      }
+    case SET_FILTERS:
+      const appliedFilters = action.filters;
+      const updatedFilteredMeals = state.meals.filter((meal) => {
+        if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+          return false;
+        }
+        if (appliedFilters.lactosFree && !meal.isLactoseFreeFree) {
+          return false;
+        }
+        if (appliedFilters.vegan && !meal.isVegan) {
+          return false;
+        }
+        if (appliedFilters.vegetarian && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      });
+      return { ...state, filteredMeals: updatedFilteredMeals };
+    default:
+      return state;
+  }
+
   return state;
 };
 
